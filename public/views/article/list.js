d7a8604e4e03b491b265c25393fe9d98da3623c7
Fred Chien <cfsghost@gmail.com>
2013-12-29 10:56:26 +0800

    add timestamp to article list

--- public/views/article/list.js
+++ public/views/article/list.js
@@ -6,12 +6,22 @@ App.require('Article', function() {
 
 		articles.forEach(function(doc, index, arr) {
 
+			var now = new Date(doc.updated);
+			var pad = function(num) {
+				return ('00' + num).slice(-2);
+			};
+			var ts = [
+				now.getFullYear() + '-' + pad(now.getMonth() + 1) + '-' + pad(now.getDate()),
+				pad(now.getHours()) + ':' + pad(now.getMinutes())
+			];
+
 			var $content = $('<div>').addClass('content');
 			var $header = $('<div>').addClass('header').text(doc.subject);
+			var $timestamp = $('<div>').addClass('description').text(ts.join(' '));
 			var $link = $('<a>').attr('href', '/edit_article/' + doc._id).addClass('item');
 			var $publish_label = $('<div>').addClass('ui label red right floated').text('Not yet published');
 
-			$content.append($header);
+			$content.append($header).append($timestamp);
 			$link
 				.append($content)
 				.append($publish_label);
