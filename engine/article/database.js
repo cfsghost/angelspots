09c26f79fbc38a14e5c12686c5ef0f9c480ce83a
Fred Chien <cfsghost@gmail.com>
2014-01-06 06:21:46 +0800

    set value to article fields by default

--- engine/article/database.js
+++ engine/article/database.js
@@ -7,11 +7,11 @@ var model = {
 
 	schema: new DBHouse.Schema({
 		_id: { type: 'UUID' },
-		author: { type: 'String' },
+		author: { type: 'String', default: '' },
 		author_id: { type: 'UUID' },
-		subject: { type: 'String' },
-		content: { type: 'String' },
-		html: { type: 'String' },
+		subject: { type: 'String', default: '' },
+		content: { type: 'String', default: '' },
+		html: { type: 'String', default: '' },
 		published: { type: 'Boolean', default: false },
 		updated: { type: 'Date', default: Date.now },
 		created: { type: 'Date', default: Date.now }
