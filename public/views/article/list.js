c332d5ceedf06b67e008bbf843e7ba183417b1cc
Fred Chien <cfsghost@gmail.com>
2014-01-16 16:15:35 +0800

    replace state label with button

--- public/views/article/list.js
+++ public/views/article/list.js
@@ -15,23 +15,37 @@ App.require('Article', function() {
 				pad(now.getHours()) + ':' + pad(now.getMinutes())
 			];
 
+			var $item = $('<div>').addClass('ui item grid');
+			var $column = $('<div>').addClass('fourteen column wide');
 			var $content = $('<div>').addClass('content');
 			var $header = $('<div>').addClass('header').text(doc.subject);
 			var $timestamp = $('<div>').addClass('description').text(ts.join(' '));
-			var $link = $('<a>').attr('href', '/edit_article/' + doc._id).addClass('item');
-			var $state_label = $('<div>').addClass('ui label right floated');
+			var $link = $('<a>').attr('href', '/edit_article/' + doc._id);
+
+			var $control_column = $('<div>').addClass('two column wide');
+			var $state_button = $('<div>').addClass('ui button right floated horizontal');
 
 			if (doc.published)
-				$state_label.addClass('green').text('Published');
+				$state_button.addClass('green').text('Published');
 			else
-				$state_label.addClass('purple').text('Draft');
+				$state_button.addClass('purple').text('Draft');
 
-			$content.append($header).append($timestamp);
+			$content
+				.append($header)
+				.append($timestamp);
+
 			$link
-				.append($content)
-				.append($state_label);
+				.append($content);
 
-			$('#article_list').append($link);
+			$column.append($link);
+
+			$control_column.append($state_button);
+
+			$item
+				.append($column)
+				.append($control_column);
+
+			$('#article_list').append($item);
 		});
 	});
 });
